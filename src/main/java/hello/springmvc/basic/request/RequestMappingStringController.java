a8dee1a5c3775d3034ad909668f73fce9fc30f69
emrhssla <emrhssla@gmail.com>
2022-02-23 22:35:33 +0900

    HTTP Method BOdy - 단순텍스트

---
+++ src/main/java/hello/springmvc/basic/request/RequestMappingStringController.java
@@ -0,0 +1,79 @@
+package hello.springmvc.basic.request;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+
+@Slf4j
+@Controller
+public class RequestMappingStringController {
+
+    /**
+     * ServletInputStream : HTTP 요청 메시지 바디의 내용을 직접 조회
+     */
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        log.info("messageBody={}", messageBody);
+
+        response.getWriter().write("ok");
+    }
+
+    /**
+     * @Controller 에서는 InputStream인자를 입력받을수있고
+     * InputStream은 HTTP 요청 메시지 바디의 내용을 직접 조회한다
+     */
+    @PostMapping("/request-body-string-v2")
+    public void requestBodyStringV2(InputStream inputStream, Writer responseWriter) throws IOException {
+
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+        log.info("messageBody={}", messageBody);
+
+        responseWriter.write("ok");
+    }
+
+    /**
+     * HttpEntity: HTTP header, body 정보를 편리하게 조회
+     * - 메시지 바디 정보를 직접 조회(요청파라미터과는 다른 방식 즉, @RequestParam X, @ModelAttribute X)
+     * - HttpMessageConverter 사용 -> StringHttpMessageConverter 적용
+     * - Post - Form 방식이어도 조회 X
+     *
+     */
+    @PostMapping("/request-body-string-v3")
+    public HttpEntity<String> requestBodyStringV3(HttpEntity<String> httpEntity) throws IOException {
+
+        String messageBody = httpEntity.getBody();
+        log.info("messageBody={}", messageBody);
+
+        // 응답에서도 HttpEntity 사용 가능
+        // - 메시지 바디 정보 직접 반환
+        return new HttpEntity<>("ok");
+    }
+
+    /**
+     * getBody() 조차 쓰기 싫다면?
+     * 이렇게 메시지 바디를 직접 조회하는 기능은
+     * 요청 파라미터를 조회하는 @RequestParam , @ModelAttribute 와는 전혀 관계가 없다.
+     */
+    @ResponseBody
+    @PostMapping("/request-body-string-v4")
+    public String requestBodyStringV4(@RequestBody String messageBody) {
+        log.info("messageBody={}", messageBody);
+        return "ok";
+    }
+}
